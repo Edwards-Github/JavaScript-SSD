8e7c05ef2312d6fb90b9b82b26e5124122596db5
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2025-04-21 15:13:08 -0700

    Finished exercise 10h

---
+++ Exercises/Lesson 10 HTML, CSS, JavaScript/10f/10f.js
@@ -0,0 +1,10 @@
+function toggleButton(className){
+  const buttonElement = document.querySelector(`.${className}`);
+
+  if(!buttonElement.classList.contains('is-toggled')) {
+    buttonElement.classList.add('is-toggled');
+  }
+  else {
+    buttonElement.classList.remove('is-toggled');
+  }
+}
